725a08dd1904cd44240a8f78ada96f56d46dbd93
greenmachine1 <scary_penguins05@hotmail.com>
2013-11-25 23:24:15 -0800

    implemented full screen mode

--- Week_1/src/com/Cory/week_1/MainActivity.java
+++ Week_1/src/com/Cory/week_1/MainActivity.java
@@ -3,16 +3,26 @@ package com.Cory.week_1;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+		WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
         setContentView(R.layout.main);
         
+        /*
         // creates a uri number
         Uri number = Uri.parse("tel:2093523633");
         
@@ -22,7 +32,7 @@ public class MainActivity extends Activity {
         // starts the activity
         startActivity(callIntent);
         
-        
+        */
         
     }
 
