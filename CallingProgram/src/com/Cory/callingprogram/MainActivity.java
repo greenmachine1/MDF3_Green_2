c52d6534eedd0fc21693df880ec7f159ec181a27
greenmachine1 <scary_penguins05@hotmail.com>
2013-12-01 01:13:03 -0800

    working on my calling program

--- CallingProgram/src/com/Cory/callingprogram/MainActivity.java
+++ CallingProgram/src/com/Cory/callingprogram/MainActivity.java
@@ -3,6 +3,9 @@ package com.Cory.callingprogram;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,26 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        
+        // creating a button to potentially call the other program
+        Button button = (Button)findViewById(R.id.button1);
+        button.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+        	
+        });
+        
+        
+        
+        
+        
+        
+        
     }
 
 
