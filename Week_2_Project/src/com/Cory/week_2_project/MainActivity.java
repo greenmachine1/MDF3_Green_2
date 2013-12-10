0c4bfa87ca5eed4e01c28e3e09b32b8969263084
greenmachine1 <scary_penguins05@hotmail.com>
2013-12-09 17:54:23 -0800

    going to start on getting gps to work

--- Week_2_Project/src/com/Cory/week_2_project/MainActivity.java
+++ Week_2_Project/src/com/Cory/week_2_project/MainActivity.java
@@ -13,7 +13,13 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+        
+        
+        
+        
+        
+        
     }
 
 
