308fdd0988c648fcd61e1bed29795c9698f28db1
greenmachine1 <scary_penguins05@hotmail.com>
2013-12-14 21:20:02 -0800

    adding sweek 3s stuff

---
+++ Week_3_Stuff/src/com/Cory/week_3_stuff/MainActivity.java
@@ -0,0 +1,59 @@
+package com.Cory.week_3_stuff;
+
+
+import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+
+public class MainActivity extends Activity{
+
+	EditText textView;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        /*
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
+        */
+
+        
+        textView = (EditText)findViewById(R.id.editText1);
+        
+        Button goButton = (Button)findViewById(R.id.button1);
+        goButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				String textThatWasEntered = textView.getText().toString();
+				Log.i("Text that was entered", textThatWasEntered);
+				
+				
+				
+				
+			}
+		});
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
