21fc9b4158502a8bc7bc7d80de0bb65453331590
greenmachine1 <scary_penguins05@hotmail.com>
2013-12-14 21:30:47 -0800

    set up actions for those action buttons

--- Week_3_Stuff/src/com/Cory/week_3_stuff/MainActivity.java
+++ Week_3_Stuff/src/com/Cory/week_3_stuff/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -46,6 +47,41 @@ public class MainActivity extends Activity{
 			}
 		});
 
+    }
+    
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+    	
+    	switch (item.getItemId()){
+    		
+    	case R.id.first:
+    		openFirst();
+    		return true;
+    	case R.id.second:
+    		openSecond();
+    		return true;
+    	case R.id.third:
+    		openThird();
+    		return true;
+    	default:
+    		return super.onOptionsItemSelected(item);
+    	
+    	
+    	}
+    	
+    }
+    
+    public void openFirst(){
+    	Log.i("first", "selected");
+    }
+    
+    public void openSecond(){
+    	Log.i("second", "selected");
+    }
+    
+    public void openThird(){
+    	Log.i("third", "selected");
     }
 
 
