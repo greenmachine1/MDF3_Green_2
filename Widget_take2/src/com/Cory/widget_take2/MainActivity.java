ee4a5ec18ce724e7fda17815cd8dc863bcd2b300
greenmachine1 <scary_penguins05@hotmail.com>
2013-12-16 20:19:28 -0800

    implementing action bar actions

--- Widget_take2/src/com/Cory/widget_take2/MainActivity.java
+++ Widget_take2/src/com/Cory/widget_take2/MainActivity.java
@@ -2,7 +2,9 @@ package com.Cory.widget_take2;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 
 // this will house the main loading screen.
@@ -30,6 +32,27 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	
+	
+	// method used to handle action bar selection
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		
+		switch (item.getItemId()){
+		case R.id.first:
+			// do something here
+			Log.i("First action bar selected", "True");
+			return true;
+		case R.id.about:
+			// do something here
+			Log.i("About action bar selected", "True");
+			return true;
+		default:
+				return super.onOptionsItemSelected(item);
+		}
+		
 	}
 
 }
