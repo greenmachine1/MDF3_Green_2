2e9c3ad80eb81508ffd2b51d893f7323699fb3f1
greenmachine1 <scary_penguins05@hotmail.com>
2013-12-18 03:55:22 -0800

    added my About activity

---
+++ Widget_take2/src/com/Cory/widget_take2/About.java
@@ -0,0 +1,19 @@
+package com.Cory.widget_take2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+// my about me section :)
+public class About extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+	}
+	
+	
+	
+	
+}
